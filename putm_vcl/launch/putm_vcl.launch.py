4e305fa5f81f34c0bdefb250ed212c90884a39da
Adam <Adam_Wasilewski_99@proton.me>
2026-05-12 19:37:14 +0200

    Added something to putm_vcl.launch.py probably tasksets idk

--- putm_vcl/launch/putm_vcl.launch.py
+++ putm_vcl/launch/putm_vcl.launch.py
@@ -10,30 +10,36 @@ def generate_launch_description():
                 package="putm_vcl",
                 executable="can_rx_node",
                 name="can_rx_node",
+		        prefix=['taskset -c 2 chrt -f 90 ']
             ),
             launch_ros.actions.Node(
                 namespace="putm_vcl",
                 package="putm_vcl",
                 executable="can_tx_node",
                 name="can_tx_node",
+		        prefix=['taskset -c 2 chrt -f 90 ']
             ),
             launch_ros.actions.Node(
                 namespace="putm_vcl",
                 package="putm_vcl",
                 executable="amk_node",
                 name="amk_node",
+                prefix=['taskset -c 3 ']
+
             ),
             launch_ros.actions.Node(
                 namespace="putm_vcl",
                 package="putm_vcl",
                 executable="rtd_node",
                 name="rtd_node",
+                prefix=['taskset -c 3 ']
             ),
             launch_ros.actions.Node(
                 namespace="putm_vcl",
                 package="putm_vcl",
                 executable="rec_node",
                 name="rec_node",
+                prefix=['taskset -c 3 ']
             ),
         ]
     )
